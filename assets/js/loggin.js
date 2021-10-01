89cddf88b6f8ec5a6b31b37ae162b0046e5ed1c3
Roman Kolnogorov <47493909+UrbanGhosts@users.noreply.github.com>
2021-10-01 19:15:46 +0500

    Add files via upload

--- assets/js/loggin.js
+++ assets/js/loggin.js
@@ -4,9 +4,8 @@ $("#enter").on("click", function () {
 	var password = $("#password").val();
 	
 	$.ajax({
-		url: 'assets/js/test1.php',
-		//url: 'assets/js/test1.js',
-		type: 'GET',
+		url: 'assets/js/test.php',
+		type: 'POST',
 		cache: false,
 		data: { 'name': name, 'password': password },
 		dataType: 'text',
